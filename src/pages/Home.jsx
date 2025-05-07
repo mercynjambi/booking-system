ee7ed03ee26578be9053a7b6a6533f50cac0a8f6
MERCY NJAMBI <njambim695@gmail.com>
2025-05-07 14:06:31 +0300

    updated offers section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedDestinations from '../components/FeaturedDestinations';
+import ExclusiveOffers from '../components/ExclusiveOffers';
 
 const Home = () => {
   return (
     <>
        <Hero /> 
        <FeaturedDestinations /> 
+       <ExclusiveOffers />
     </>
   )
 }
