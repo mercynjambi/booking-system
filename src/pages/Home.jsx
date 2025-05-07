6643163a62652b45ee6c9624f033cd0fa6352d1c
MERCY NJAMBI <njambim695@gmail.com>
2025-05-07 23:02:05 +0300

    ceated testimonials

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedDestinations from '../components/FeaturedDestinations';
 import ExclusiveOffers from '../components/ExclusiveOffers';
+import Testimonials from '../components/Testimonials';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
        <Hero /> 
        <FeaturedDestinations /> 
        <ExclusiveOffers />
+       <Testimonials/>
     </>
   )
 }
