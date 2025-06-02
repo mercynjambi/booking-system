65a26684cc1f32441d2a013f4312fb47cba69568
MERCY NJAMBI <njambim695@gmail.com>
2025-06-03 00:12:21 +0300

    updated room listimgs

--- src/pages/hotelOwner/Layout.jsx
+++ src/pages/hotelOwner/Layout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div className='flex flex-col h-screen'>
+    <div className='flex flex-col min-h-screen'>
         <Navbar/>
         <div className='flex h-full'>
             <Sidebar/>
